5c16847e04ccd309e332ec9b7c72f1e205ad2d66
Victor <nicromante656@hotmail.com>
2022-10-17 14:19:10 +0200

    Xml

---
+++ Ud3_XML/src/main/java/org/iesfm/DAO/FileDao.java
@@ -0,0 +1,10 @@
+package org.iesfm.DAO;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+import java.util.Map;
+
+public interface FileDao {
+
+}
