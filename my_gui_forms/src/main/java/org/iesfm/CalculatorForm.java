be8749aff275640dc9f6669af2f38e8946943a3a
Victor <nicromante656@hotmail.com>
2022-10-18 11:38:09 +0200

    Gui Forms modificaciones

--- my_gui_forms/src/main/java/org/iesfm/CalculatorForm.java
+++ my_gui_forms/src/main/java/org/iesfm/CalculatorForm.java
@@ -6,6 +6,24 @@ public class CalculatorForm {
     private JPanel mainPanel;
     private JPanel northPanel;
     private JTextField a0TextField;
+    private JPanel centerPanel;
+    private JPanel southPanel;
+    private JButton a7Button;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton DELButton;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton button8;
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton button12;
+    private JButton button13;
+    private JButton a0Button;
+    private JButton button15;
+    private JButton xButton;
 
     public static void main(String[] args) {
 
